174fbef9df9c1c5e24f887b109d6596b7d6e150c
jamrockshuffle <info.dennis.1010@googlemail.com>
2025-12-24 23:06:36 +0100

    Review Dateien hinzugefügt
    Die Review Dateien enthalten alle Übungsfragen des jeweiligen Kapitels.

---
+++ OLD_DATA/OLD_DATA/Chapter 1/Chapter1.1.cpp
@@ -0,0 +1,13 @@
+// Date 19/12/2025
+// File Chapter1.1.cpp
+//
+
+// [1] Change output.
+
+#include "PPPheaders.h"
+
+static int helloProgramming() {
+
+	cout << "Hello, Programming!\nHere we go!\n";
+	return 0;
+}
---
+++ OLD_DATA/OLD_DATA/Chapter 1/Chapter1.3.cpp
@@ -0,0 +1,24 @@
+// Date 19/12/2025
+// File Chapter1.3.cpp
+//
+
+// [3] Write a description of how to get from your front door to a target of your choosing
+
+#include "PPPheaders.h"
+
+static int giveDirections() {
+
+	// target: seesaws outside my building
+	cout << "From the front door, turn 90 degrees to the right and walk until past the wall on your left.\n";
+	cout << "Then turn 90 degrees left and walk until the elevator buttons are just to your left.\n";
+	cout << "Turn 90 degrees left again and press one of the two buttons, so that a red light switches on behind the button.\n";
+	cout << "Take a couple of steps back and wait until either the left or the right elevator opens its doors.\n";
+	cout << "Step into that elevator and turn 90 right to face the control pannel. Press the button labeled 'E'.\n";
+	cout << "Optional: Press the button labeled \"><\", to make the elevator doors close faster.\n";
+	cout << "Turn 90 degrees right, to face the elevator doors and wait.\n";
+	cout << "Once they have opened again, step out and turn 90 degrees left.\n";
+	cout << "Walk forward until you've passed the wall on your left, then turn 90 degrees left again.\n";
+	cout << "Now walk forward until you've reached the door to the outside, place your hand upon its handle, press it down, then push the door open and step out.\n";
+	cout << "Turn 45 degrees left and walk to narrow pathway to its end.\n";
+	cout << "Once done, the seesaws will be located on your left, have fun!\n";
+}
